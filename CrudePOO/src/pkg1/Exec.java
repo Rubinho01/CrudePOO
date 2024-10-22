66676c5771f8c730ce4e379b40a52d7648b569da
Rubinho01 <rubemkruger123@gmail.com>
2024-10-22 19:43:25 -0300

    Criação do Menu Infinito

--- CrudePOO/src/pkg1/Exec.java
+++ CrudePOO/src/pkg1/Exec.java
@@ -9,6 +9,20 @@ public class Exec {
 		conexao.getConexaoMySQL();
 		
 		int escolha;
-	}
+		
+		Scanner scanner = new Scanner(System.in);
+		do {
+		System.out.println("Seja Bem-vindo");
+		System.out.println("1. Adicionar novo Associado");
+		System.out.println("2. Ver lista de Associados ");
+		System.out.println("3. Excluir Associado");
+		System.out.println("4. Encerrar Programa");
+		System.out.println("Digite a opção: ");
+		escolha = scanner.nextInt();
+		
+	}while(escolha!=4);
+		System.out.println("Saindo....");
+		scanner.close();
 
+}
 }
