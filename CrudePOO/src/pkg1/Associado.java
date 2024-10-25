ec73b563c5af76ad98bb75110cb074f293cc0ddf
Rubinho01 <rubemkruger123@gmail.com>
2024-10-25 13:24:12 -0300

    troca do tipo de atributo cpf de long para String

--- CrudePOO/src/pkg1/Associado.java
+++ CrudePOO/src/pkg1/Associado.java
@@ -4,12 +4,30 @@ package pkg1;
 
 import java.math.*;
 public abstract class Associado {
-	private long cpfAssociado;
+	private String cpfAssociado;
 	private String nomeAssociado;
 	private String emailAssociado;
-	public Associado(long cpfAssociado, String nomeAssociado, String emailAssociado) {
+	public Associado(String cpfAssociado, String nomeAssociado, String emailAssociado) {
 		this.cpfAssociado = cpfAssociado;
 		this.nomeAssociado = nomeAssociado;
+		this.emailAssociado = emailAssociado;
+	}
+	public String getCpfAssociado() {
+		return cpfAssociado;
+	}
+	public void setCpfAssociado(String cpfAssociado) {
+		this.cpfAssociado = cpfAssociado;
+	}
+	public String getNomeAssociado() {
+		return nomeAssociado;
+	}
+	public void setNomeAssociado(String nomeAssociado) {
+		this.nomeAssociado = nomeAssociado;
+	}
+	public String getEmailAssociado() {
+		return emailAssociado;
+	}
+	public void setEmailAssociado(String emailAssociado) {
 		this.emailAssociado = emailAssociado;
 	}
 
