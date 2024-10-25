f62a536cde84c2c63bf97e57bfdb28c7f8cbed34
Rubinho01 <rubemkruger123@gmail.com>
2024-10-25 13:22:52 -0300

    Iniciando Processo de instanciar objeto na db

--- CrudePOO/src/pkg1/CrudMensalista.java
+++ CrudePOO/src/pkg1/CrudMensalista.java
@@ -1,5 +1,21 @@
 package pkg1;
-
+import java.sql.*;
 public class CrudMensalista {
-
+	Connection conn = null;
+	public void inserirMensalista(associadoMensalista mensalista) {
+		String sql = "INSERT INTO mensalistas (cpf_mensalista, nome_mensalista, email_mensalista, mensalidade) VALUES (?,?,?,?)";
+		
+		try(Connection conn = ConexaoMySQL.getConexaoMySQL();
+				PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, mensalista.getCpfAssociado());
+            stmt.setString(2, mensalista.getNomeAssociado());
+			
+				
+			
+		}catch (SQLException e) {
+            System.out.println("Erro ao cadastrar Associado");
+			e.printStackTrace();
+        }
+			
+	}
 }
