c31b3e5cd24717fc18c1c53fdc85b981604b0f7b
Rubinho01 <rubemkruger123@gmail.com>
2024-10-26 20:13:12 -0300

    método de visualizar mensalistas

--- CrudePOO/src/pkg1/CrudMensalista.java
+++ CrudePOO/src/pkg1/CrudMensalista.java
@@ -9,6 +9,11 @@ public class CrudMensalista {
 				PreparedStatement stmt = conn.prepareStatement(sql)) {
             stmt.setString(1, mensalista.getCpfAssociado());
             stmt.setString(2, mensalista.getNomeAssociado());
+            stmt.setString(3, mensalista.getEmailAssociado());
+            stmt.setBigDecimal(4, mensalista.getMensalidadeMensalista());
+            
+            stmt.executeUpdate();
+            System.out.println("Associado Mensalista cadastrado com sucesso!");
 			
 				
 			
@@ -18,4 +23,26 @@ public class CrudMensalista {
         }
 			
 	}
+	public void listarMensalistas() {
+        String sql = "SELECT cpf_mensalista, nome_mensalista, email_mensalista, mensalidade FROM mensalistas";
+
+        try (Connection conn = ConexaoMySQL.getConexaoMySQL();
+             PreparedStatement stmt = conn.prepareStatement(sql);
+             ResultSet rs = stmt.executeQuery()) {
+
+            // Iterando sobre o ResultSet e imprimindo os dados
+            while (rs.next()) {
+                String cpf = rs.getString("cpf_mensalista");
+                String nome = rs.getString("nome_mensalista");
+                String email = rs.getString("email_mensalista");
+                double mensalidade = rs.getDouble("mensalidade");
+
+                // Exibindo os dados no console
+                System.out.println("CPF: " + cpf + ", Nome: " + nome + ", Email: " + email + ", Mensalidade: " + mensalidade);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
